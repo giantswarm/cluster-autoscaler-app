c7cb52c1a8abf9d3838652bb1a198962b66a7f1a
Ross Fairbanks <ross@rossfairbanks.com>
2020-06-05 14:57:10 +0200

    Fix values

--- integration/templates/cluster_autoscaler_values.go
+++ integration/templates/cluster_autoscaler_values.go
@@ -3,5 +3,4 @@
 package templates
 
 // ClusterAutoscalerValues values used for cluster-autoscaler-app in integration test
-const ClusterAutoscalerValues = `e2e: true
-provider: none`
+const ClusterAutoscalerValues = `e2e: true`
